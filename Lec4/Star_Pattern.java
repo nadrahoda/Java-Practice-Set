31af67f507cd989071993f8acffd664ef0bceabc
nadrahoda <nadrahoda2412@gmail.com>
2023-01-28 01:36:43 +0530

    updated

--- Lec4/Star_Pattern.java
+++ Lec4/Star_Pattern.java
@@ -1,6 +1,21 @@
 package Lec4;
 import java.util.Scanner;
 public class Star_Pattern {
-    Scanner s = new Scanner(System.in);
+    public static void main(String[] args) {
+        
     
+    Scanner s = new Scanner(System.in);
+    int n = s.nextInt();
+    int i=1;
+    while(i<=n){
+        int j=1;
+        while(j<=n){
+            System.out.print("*");
+            j++;
+
+        }
+        System.out.println();
+        i++;
+    }
+}
 }
