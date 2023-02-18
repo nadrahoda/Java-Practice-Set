2ee097b7572c837729dfcda9871bb67c9b8974df
nadrahoda <nadrahoda2412@gmail.com>
2023-02-18 23:41:07 +0530

    fibonacci

---
+++ Operators_And_For_Loop/Fibonacci.java
@@ -0,0 +1,22 @@
+package Operators_And_For_Loop;
+
+// Fibonacci Number
+
+import java.util.Scanner;
+public class Fibonacci {
+    public static void main(String[] args) {
+    Scanner s = new Scanner(System.in);
+    int n = s.nextInt();
+
+    int a =0;
+    int b=1;
+
+    for(int i=1; i<=n ; i++ ){
+        int c = a+b;
+        a=b;
+        b=c;
+        
+    }
+    System.out.print(a + " ");
+}
+}
